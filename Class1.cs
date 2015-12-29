88ea3d4bc6d43bf3cfd4aa503399d56f9609f1d0
TwoZeros <pak.kiruha@yandex.ru>
2015-12-29 14:43:28 +0300

    Должно всё работать
    Не так ли

---
+++ Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+namespace ComPortApplication
+{
+    class MyPort
+    {
+        int ValueSpeed;
+       static SerialPort port;
+        public MyPort(string namePort, int IndexSpeed)
+        {
+            switch (IndexSpeed)
+            {
+                case 0:
+                        ValueSpeed = 2400;
+                    break;
+                case 1:
+                    ValueSpeed = 4800;
+                    break;
+                case 2:
+                    ValueSpeed = 19200;
+                    break;
+                    
+            }
+             port = new SerialPort(namePort, ValueSpeed, Parity.None, 8, StopBits.One);
+
+
+        }
+        public void OpenPort()
+        {
+            port.Open();
+
+        }
+        public static void SendMessege(Byte[] data)
+        {
+            port.Write(data, 0, data.Length);
+            System.Threading.Thread.Sleep(300);
+        }
+        public static string answer()
+        {
+
+            
+            if (port.BytesToRead > 0)
+            {
+                Byte[] answer = new byte[port.BytesToRead];     //  Читаем буфер для аналаза ответа на команду Z (управление)
+                port.Read(answer, 0, port.BytesToRead);
+                string otvet = "";
+                foreach (byte elem in answer)
+                {
+                    otvet += elem.ToString() + " ";
+                }
+                return otvet;
+            }
+            else
+                return "нет ответа"; 
+        }
+        static public byte[] codder(string Massege)
+        {
+            String[] StringData = Massege.Split(' ');
+            Byte[] Data = new Byte[StringData.Length];
+            int i = 0;
+            foreach (string elem in StringData)
+            {
+                Data[i] = Convert.ToByte(elem, 16);
+                ++i;
+        
+            }
+            return Data;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+namespace ComPortApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string Massege;
+        byte[] Data;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            ListPort.ItemsSource = SerialPort.GetPortNames();
+            SpeedPort.ItemsSource = new string[] { "2400", "4800", "19200" };
+            ClosePort.IsEnabled = false;
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            Massege= InputBox.Text;
+           Data=MyPort.codder(Massege);
+            MyPort.SendMessege(Data);
+           Status_Controller.Text=MyPort.answer();
+        }
+
+        private void ClosePort_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListPort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void OpenPort_Click(object sender, RoutedEventArgs e)
+        {
+            int IndexSpeed = SpeedPort.SelectedIndex;
+            string namePort = ListPort.SelectedItem.ToString();
+            MyPort GoPort = new MyPort(namePort, IndexSpeed);
+            GoPort.OpenPort();
+            OpenPort.IsEnabled = false;
+            ClosePort.IsEnabled = true;
+        }
+    }
+}
